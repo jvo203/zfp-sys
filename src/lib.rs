24d48ea2126b53e01b94ee8f2c9e4242f36a3a14
jvo203 <chris.zapart@nao.ac.jp>
2019-02-12 13:40:54 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zfp_stream() {
+        assert_eq!(2 + 2, 4);
+    }
+}
